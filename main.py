3ad7cf30309962086c0be05c37af211e3503d4cb
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-06-10 12:41:10 +0200

    feat: option to print ast

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from src.generation_code import gen_entrypoint
 
 def main(args):
     if len(args) < 2:
-        print("usage: python3 analyse_syntaxique.py NOM_FICHIER_SOURCE.flo")
+        print(
+            "usage: python3 analyse_syntaxique.py NOM_FICHIER_SOURCE.flo [--ast]]"
+        )
     else:
         with open(args[1], "r") as f:
             data = f.read()
             try:
                 arbre = analyse_syntaxique(data)
-                gen_entrypoint(arbre)
+                if len(args) > 2 and args[2] == "--ast":
+                    print(arbre)
+                else:
+                    gen_entrypoint(arbre)
             except EOFError:
                 exit()
 
